8d8d2d5b59a03b9d735fdc753c42750a45541c31
Adrian Mihai <adrian@carve.ro>
2015-04-11 10:37:42 +0000

    added phpunit to dev dependencies

--- src/Carvefx/Analyzer/Analyzer.php
+++ src/Carvefx/Analyzer/Analyzer.php
@@ -12,29 +12,33 @@ class Analyzer
   private $duration;
   private $interval;
 
-  public function __construct(ProcessListStreamInterface $process_list, OutputInterface $output, $duration = 60, $interval = 1)
-  {
+  public function __construct(
+    ProcessListStreamInterface $process_list,
+    OutputInterface $output,
+    $duration = 60,
+    $interval = 1
+  ) {
     $this->process_list = $process_list;
     $this->output = $output;
     $this->duration = $duration;
     $this->interval = $interval;
   }
 
-    public function fire()
-    {
-        while($this->duration) {
-          $list = $this->process_list->get();
+  public function fire()
+  {
+    while ($this->duration) {
+      $list = $this->process_list->get();
 
-          foreach($list as $item) {
-            $this->output->write($item);
-          }
+      foreach ($list as $item) {
+        $this->output->write($item);
+      }
 
-          $this->process_list->refresh();
-          $this->decreaseDuration();
-        }
-
-      $this->output->dump();
+      $this->process_list->refresh();
+      $this->decreaseDuration();
     }
+
+    $this->output->dump();
+  }
 
   /*
   private function getTimestamp()
